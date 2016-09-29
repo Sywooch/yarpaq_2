14b6af8ac17eb655ad7f0588d8c0e6c5145cf4b3
Timur <t-moor@live.ru>
2016-09-30 01:44:05 +0400

    amnah/yii2-user installed and configured

--- common/config/main.php
+++ common/config/main.php
@@ -5,5 +5,14 @@ return [
         'cache' => [
             'class' => 'yii\caching\FileCache',
         ],
+        'user' => [
+            'class' => 'amnah\yii2\user\components\User',
+        ],
+    ],
+    'modules' => [
+        'user' => [
+            'class' => 'amnah\yii2\user\Module',
+            // set custom module properties here ...
+        ],
     ],
 ];
