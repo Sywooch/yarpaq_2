c8c7dd1b92d6b9b85c01e18a8677a28412cc2b0b
Timur <t-moor@live.ru>
2016-09-30 01:27:29 +0400

    vagrant provision updated

--- vagrant/provision/once-as-vagrant.sh
+++ vagrant/provision/once-as-vagrant.sh
@@ -35,7 +35,7 @@ info "Init project"
 ./init --env=Development --overwrite=y
 
 info "Apply migrations"
-./yii migrate <<< "yes"
+./yii migrate <<< "no"
 
 info "Create bash-alias 'app' for vagrant user"
 echo 'alias app="cd /app"' | tee /home/vagrant/.bash_aliases
