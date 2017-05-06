c1c1caaec7ec0faa2249b5caa0c197655f3af6ae
Timur <t-moor@live.ru>
2017-05-06 17:50:59 +0400

    config correction

--- common/config/params.php
+++ common/config/params.php
@@ -3,9 +3,4 @@ return [
     'adminEmail' => 'admin@example.com',
     'supportEmail' => 'support@example.com',
     'user.passwordResetTokenExpire' => 3600,
-    'uploadsPath' => dirname(dirname(__DIR__)) . '/frontend/web/uploads',
-    'uploadsDir' => 'uploads',
-
-    'product.uploads.url' => '/image/catalog/',
-    'product.uploads.path' => dirname(dirname(__DIR__)) . '/frontend/web/uploads/image/catalog/',
 ];
