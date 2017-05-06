5b763096f821727c2416132fa49e643795a7ac82
Timur <t-moor@live.ru>
2017-05-06 22:35:52 +0400

    left icons fixed

--- backend/views/layouts/left.php
+++ backend/views/layouts/left.php
@@ -25,23 +25,23 @@ use yii\helpers\Url;
                 'items' => [
                     ['label' => 'Menu', 'options' => ['class' => 'header']],
                     ['label' => 'Products', 'url' => ['/product']],
-                    ['label' => 'Categories', 'icon' => 'fa fa-sitemap', 'url' => ['/category']],
-                    ['label' => 'Manufacturers', 'icon' => 'fa fa-tags', 'url' => ['/manufacturer']],
-                    ['label' => 'Currencies', 'icon' => 'fa fa-money', 'url' => ['/currency']],
-                    ['label' => 'Languages', 'icon' => 'fa fa-language', 'url' => ['/language']],
-                    ['label' => 'Users', 'icon' => 'fa fa-users', 'url' => ['#'], 'items' => [
-                        ['label' => UserManagementModule::t('back', 'Users'), 'url' => ['/user-management/user/index'], 'icon' => 'fa fa-angle-double-right'],
-                        ['label' => UserManagementModule::t('back', 'Roles'), 'url' => ['/user-management/role/index'], 'icon' => 'fa fa-angle-double-right'],
-                        ['label' => UserManagementModule::t('back', 'Permissions'), 'url' => ['/user-management/permission/index'], 'icon' => 'fa fa-angle-double-right'],
-                        ['label' => UserManagementModule::t('back', 'Permission groups'), 'url' => ['/user-management/auth-item-group/index'], 'icon' => 'fa fa-angle-double-right'],
-                        ['label' => UserManagementModule::t('back', 'Visit log'), 'url' => ['/user-management/user-visit-log/index'], 'icon' => 'fa fa-angle-double-right'],
+                    ['label' => 'Categories', 'icon' => 'sitemap', 'url' => ['/category']],
+                    ['label' => 'Manufacturers', 'icon' => 'tags', 'url' => ['/manufacturer']],
+                    ['label' => 'Currencies', 'icon' => 'money', 'url' => ['/currency']],
+                    ['label' => 'Languages', 'icon' => 'language', 'url' => ['/language']],
+                    ['label' => 'Users', 'icon' => 'users', 'url' => ['#'], 'items' => [
+                        ['label' => UserManagementModule::t('back', 'Users'), 'url' => ['/user-management/user/index'], 'icon' => 'angle-double-right'],
+                        ['label' => UserManagementModule::t('back', 'Roles'), 'url' => ['/user-management/role/index'], 'icon' => 'angle-double-right'],
+                        ['label' => UserManagementModule::t('back', 'Permissions'), 'url' => ['/user-management/permission/index'], 'icon' => 'angle-double-right'],
+                        ['label' => UserManagementModule::t('back', 'Permission groups'), 'url' => ['/user-management/auth-item-group/index'], 'icon' => 'angle-double-right'],
+                        ['label' => UserManagementModule::t('back', 'Visit log'), 'url' => ['/user-management/user-visit-log/index'], 'icon' => 'angle-double-right'],
                     ]],
                     ['label' => 'Base', 'items' => [
                         ['label' => 'Options', 'url' => ['option/index']]
                     ]],
                     ['label' => 'System', 'options' => ['class' => 'header']],
-                    ['label' => 'Gii', 'icon' => 'fa fa-file-code-o', 'url' => ['/gii']],
-                    ['label' => 'Debug', 'icon' => 'fa fa-dashboard', 'url' => ['/debug']],
+                    ['label' => 'Gii', 'icon' => 'file-code-o', 'url' => ['/gii']],
+                    ['label' => 'Debug', 'icon' => 'dashboard', 'url' => ['/debug']],
                     ['label' => 'Login', 'url' => ['site/login'], 'visible' => Yii::$app->user->isGuest],
 //                    [
 //                        'label' => 'Same tools',
