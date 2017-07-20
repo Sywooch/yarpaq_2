883f24a4858f36a78dc5798d799b289418e46320
Timur <t-moor@live.ru>
2017-07-20 15:15:32 +0400

    fix

--- frontend/components/views/_tape.php
+++ frontend/components/views/_tape.php
@@ -11,16 +11,18 @@
                              title="<?= $product->title; ?>">
                     </div>
                     <h3><?= $product->title; ?></h3>
-                    <div class="price"><span>48 AZN</span></div>
+                    <div class="price"><span><?= $product->price; ?> AZN</span></div>
+                    <?php if (false) { // if hasDiscount() ?>
                     <div class="old_price">
                         <span><?= Yii::t('app', 'Qiymət');?>: <em><?= $product->price; ?> AZN</em></span>
                         <strong>73% OFF</strong>
                     </div>
+                    <?php } ?>
                     <div class="rating">
                         <span class="star_3"></span>
                     </div>
                 </div>
-                <a href="#"></a>
+                <a href="<?= $product->url; ?>"></a>
             </li>
             <?php } ?>
         </ul>
