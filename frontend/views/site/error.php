1de800661b25962e7df2084747c016055ba3405f
Timur <t-moor@live.ru>
2017-08-13 20:01:25 +0400

    404 page

--- frontend/views/site/error.php
+++ frontend/views/site/error.php
@@ -9,19 +9,10 @@ use yii\helpers\Html;
 
 $this->title = $name;
 ?>
-<div class="site-error">
-
-    <h1><?= Html::encode($this->title) ?></h1>
-
-    <div class="alert alert-danger">
-        <?= nl2br(Html::encode($message)) ?>
+<div class="page_404">
+    <div>
+        <h2>404</h2>
+        <h3>axtardığınız səhifə tapılmadı</h3>
+        <p>Return to <a href="#">HOME PAGE</a></p>
     </div>
-
-    <p>
-        The above error occurred while the Web server was processing your request.
-    </p>
-    <p>
-        Please contact us if you think this is a server error. Thank you.
-    </p>
-
 </div>
