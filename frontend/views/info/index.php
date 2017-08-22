6b13f45acf0c346da29aa27bb7dd109fc1415151
Timur <t-moor@live.ru>
2017-08-22 12:18:51 +0400

    info page

--- frontend/views/info/index.php
+++ frontend/views/info/index.php
@@ -14,35 +14,8 @@ $currency = Yii::$app->currency;
 
     </header>
 
-    <div class="product_additional">
-        <div class="product_infos">
-            <div class="tabs">
-                <ul>
-                    <li><a href="#"><?= $info->title; ?></a></li>
-                </ul>
-            </div>
-            <div class="inner">
-                <div class="active">
-                    <?= $info->content->body; ?>
-                </div>
-            </div>
-        </div>
-        <aside class="aside_products">
-            <article itemscope="" itemtype="http://schema.org/Product">
-                <div>
-                    <div class="image"><img src="upload/Images/10.jpg" alt="" itemprop="image"></div>
-                    <h3 itemprop="name">Puma Epoch Black Lifestyle Casual Shoes</h3>
-                    <div class="price"><span itemprop="price">48 <b class="currency_icon">m</b></span></div>
-                    <div class="old_price">
-                        <span>Qiymət: <em>58<b class="currency_icon">m</b></em></span>
-                        <strong>73% OFF</strong>
-                    </div>
-                    <div class="rating">
-                        <span class="star_2"></span>
-                    </div>
-                </div>
-                <a href="#"></a>
-            </article>
-        </aside>
+    <div class="info-block">
+        <h1><?= $info->title; ?></h1>
+        <?= $info->content->body; ?>
     </div>
 </div>
