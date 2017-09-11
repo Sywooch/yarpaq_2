6bc4140df72e971151dd17b467bf84ea6fa4f494
Timur <t-moor@live.ru>
2017-09-12 00:50:13 +0400

    бестселлеры только за последние 3 месяца

--- frontend/components/BestSellersTape.php
+++ frontend/components/BestSellersTape.php
@@ -12,11 +12,16 @@ class BestSellersTape extends Tape
     public $seeAllLabel = 'See All';
 
     public function loadProducts() {
+        $last_3_months = new \DateTime('-3 months');
+
         $topSellOrderProducts = OrderProduct::find('product_id IS NOT NULL')
             ->select(['COUNT(product_id) AS count, product_id'])
-            ->joinWith('product p')
+            ->joinWith('product p', 'p.id = {{%order_product}}.product_id')
             ->andWhere(['p.status_id' => Product::STATUS_ACTIVE])
             ->andWhere(['p.moderated' => 1])
+            ->joinWith('order o', 'o.id = {{%order_product}}.order_id')
+            ->andWhere(['>=', 'o.created_at', $last_3_months->format('Y-m-d H:i:s')])
+            ->andWhere(['o.order_status_id' => 23])
             ->groupBy('product_id')
             ->orderBy(['count' => SORT_DESC])
             ->limit(20)
