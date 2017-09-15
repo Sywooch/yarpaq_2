b7e1f4943821ebf9db13f937b9349a2a4cd0d057
Timur <t-moor@live.ru>
2017-09-15 14:14:03 +0400

    shipping calculate fix

--- common/models/shipping/Shipping.php
+++ common/models/shipping/Shipping.php
@@ -7,22 +7,24 @@ abstract class Shipping
     protected $rates;
 
     public function calculateCost($weight, $geo_zone_id) {
+        $result = 0;
+
         if (isset($this->rates[$geo_zone_id])) {
             $rate = $this->rates[$geo_zone_id];
 
-            foreach (explode(';', $rate) as $pair) {
+            foreach (explode(',', $rate) as $pair) {
                 $rate_weight    = explode(':', $pair)[0];
                 $rate_cost      = explode(':', $pair)[1];
 
-                if ($weight <= $rate_weight) {
-                    return $rate_cost;
-                } else {
+                if ($rate_weight > $weight) {
                     break;
+                } else {
+                    $result = $rate_cost;
                 }
             }
-        } else {
-            return false;
         }
+
+        return $result;
     }
 
     public function getAvailableGeoZones() {
