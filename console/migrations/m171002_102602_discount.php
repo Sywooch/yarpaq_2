bb0f51c8dc5a5e5b00f1a2de89abd8bc59f9173d
Timur <t-moor@live.ru>
2017-10-02 14:32:15 +0400

    discount migration

---
+++ console/migrations/m171002_102602_discount.php
@@ -0,0 +1,40 @@
+<?php
+
+use yii\db\Migration;
+
+class m171002_102602_discount extends Migration
+{
+    public function safeUp()
+    {
+        $this->createTable('{{%discount}}', [
+            'id'            => $this->primaryKey()->notNull(),
+            'product_id'    => $this->integer()->notNull(),
+            'value'         => $this->double()->notNull(),
+            'period'        => $this->smallInteger()->notNull(),
+            'start_date'    => $this->dateTime(),
+            'end_date'      => $this->dateTime()
+        ]);
+
+        $this->addForeignKey('product', '{{%discount}}', 'product_id', '{{%product}}', 'id');
+    }
+
+    public function safeDown()
+    {
+        $this->dropTable('{{%discount}}');
+    }
+
+    /*
+    // Use up()/down() to run migration code without a transaction.
+    public function up()
+    {
+
+    }
+
+    public function down()
+    {
+        echo "m171002_102602_discount cannot be reverted.\n";
+
+        return false;
+    }
+    */
+}
