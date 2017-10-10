e875c1a56c5b44f1fc9af0cfdf2d833251637b02
Timur <t-moor@live.ru>
2017-10-10 11:01:51 +0400

    sitemap категории - добавлены ссылки на все языки

--- frontend/views/sitemap/categories.php
+++ frontend/views/sitemap/categories.php
@@ -1,25 +1,30 @@
 <?php
 use yii\helpers\Url;
+use common\models\Language;
 
-echo '<?xml version="1.0" encoding="UTF-8"?>';
+$languages = Language::find()->all();
 
-echo '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">';
+echo '<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"
+xmlns:xhtml="http://www.w3.org/1999/xhtml">'.PHP_EOL;
 
 foreach ($categories as $category) {
+    echo '<url>';
+    echo '<loc>'.Url::to($category->url, true).'</loc>'.PHP_EOL;
+
+    foreach ($languages as $language) {
+        if ($language->isDefault()) continue;
+
+        echo '<xhtml:link rel="alternate" hreflang="'.$language->name.'" href="'.$category->getUrlByLanguage($language).'" />'.PHP_EOL;
+    }
+
+
     echo '
-    <url>
-
-        <loc>'.Url::to($category->url, true).'</loc>
-
         <lastmod>'.( new \DateTime($category->updated_at))->format('Y-m-d').'</lastmod>
-
         <changefreq>daily</changefreq>
-
         <priority>0.8</priority>
-
     </url>
     ';
 }
-
 
 echo '</urlset>';
