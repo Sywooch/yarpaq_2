b4eacf404b1df4145ef97104d12d55ba7d374580
Timur <t-moor@live.ru>
2017-10-23 02:58:35 +0400

    style fix

--- frontend/assets/AppAsset.php
+++ frontend/assets/AppAsset.php
@@ -18,7 +18,7 @@ class AppAsset extends AssetBundle
         'js/zoom/dist/xzoom.css',
         'js/slick/slick.css',
         'js/slick/slick-theme.css',
-        'css/main.css?v1.0.9',
+        'css/main.css?v1.0.11',
         'css/responsive.css?v=1.0.5',
         'css/common.css'
     ];
