5c8d39642006d35c0f2001eddd045687cbd0a502
Timur <t-moor@live.ru>
2017-11-24 02:12:30 +0400

    categories href lang fix

--- frontend/views/sitemap/categories.php
+++ frontend/views/sitemap/categories.php
@@ -9,20 +9,26 @@ echo '<?xml version="1.0" encoding="UTF-8"?>
 xmlns:xhtml="http://www.w3.org/1999/xhtml">'.PHP_EOL;
 
 foreach ($categories as $category) {
-    echo '<url>';
-    echo '<loc>'.Url::to($category->url, true).'</loc>'.PHP_EOL;
 
+    $urls = [];
+    $href_lang_markup = '';
     foreach ($languages as $language) {
-        echo '<xhtml:link rel="alternate" hreflang="'.$language->name.'" href="'.Url::to($category->getUrlByLanguage($language), true).'" />'.PHP_EOL;
+        $url = Url::to($category->getUrlByLanguage($language), true);
+        $urls[$language->id] = $url;
+        $href_lang_markup .= '<xhtml:link rel="alternate" hreflang="' . $language->name . '" href="' . $url . '" />' . PHP_EOL;
     }
 
-
-    echo '
-        <lastmod>'.( new \DateTime($category->updated_at))->format('Y-m-d').'</lastmod>
-        <changefreq>daily</changefreq>
-        <priority>0.8</priority>
-    </url>
-    ';
+    foreach ($languages as $language) {
+        echo '<url>';
+        echo '<loc>' . $urls[$language->id] . '</loc>' . PHP_EOL;
+        echo $href_lang_markup;
+        echo '
+            <lastmod>' . (new \DateTime($category->updated_at))->format('Y-m-d') . '</lastmod>
+            <changefreq>daily</changefreq>
+            <priority>0.8</priority>
+        </url>
+        ';
+    }
 }
 
 echo '</urlset>';
